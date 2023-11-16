4f01ed2213e8c9387f20735ed7bab19a19824fa4
dtabirca <dtabirca@1029792.no-reply.drupal.org>
2023-11-16 13:11:28 +0200

    core/once + ajax fix

--- js/dialog.js
+++ js/dialog.js
@@ -3,14 +3,14 @@
 
   Drupal.behaviors.copernicusDialog = {
     attach: function(context) {
-      $(context).find('body')
-        .once('copernicus-dialog')
-        .each(function () {
-          var ajaxSettings = {
-            url: '/copernicus-dialog/open-dialog'
-          };
-          var ajaxObject = Drupal.ajax(ajaxSettings);
-          ajaxObject.execute().done(function(){
+      const elements = once('copernicus-dialog', 'body', context);
+      elements.forEach(function (element) {
+        var ajaxSettings = {
+          url: '/copernicus-dialog/open-dialog'
+        };
+        var ajaxObject = Drupal.ajax(ajaxSettings);
+        ajaxObject.execute().done(function(){
+          setTimeout (function(){
             $('.ui-dialog-titlebar-close').bind('click', function() {
               var ajaxSettings = {
                 url: '/copernicus-dialog/close-dialog'
@@ -18,8 +18,9 @@
               var ajaxObject = Drupal.ajax(ajaxSettings);
               ajaxObject.execute();
             });
-          });
+          }, 100);
         });
+      });
     }
   };
 
